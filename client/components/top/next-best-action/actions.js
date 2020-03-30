28ceee20ec1dff62d2cd42e32cbd1b2c8aa9839f
Ben Fletcher <bjfletcher@gmail.com>
2020-03-30 21:07:18 +0100

    use myFT instead of email platform for one-click newsletter subscription 🐿 v2.12.5

--- client/components/top/next-best-action/actions.js
+++ client/components/top/next-best-action/actions.js
@@ -1,8 +1,39 @@
+import { client as myft } from 'n-myft-ui/myft';
+import getCsrfToken from 'n-myft-ui/myft/ui/lib/get-csrf-token';
 import dailyDigestSignUp from '../../../lib/daily-digest';
 
 export function newsletter (guruResult) {
-	const url = guruResult.renderData.ctaActionHref;
-	return fetch(url)
+	function getUserId () {
+		// if there's a better way than using myFT, I'd like to know!
+		return myft.init()
+			.then(function withMyFt () {
+				return myft.userId;
+			});
+	}
+	function getNewsletterId (guruResult) {
+		const urlParts = guruResult.renderData.ctaActionHref.split('/');
+		return urlParts[urlParts.length - 2];
+	}
+	function getUrl (userId, newsletterId) {
+		return `/__myft/api/alerts/${userId}/newsletters/${newsletterId}/subscribe`;
+
+	}
+
+	return getUserId()
+		.then(function subscribeToNewsletter (userId) {
+			const url = getUrl(userId, getNewsletterId(guruResult));
+			return fetch(url, {
+				method: 'POST',
+				credentials: 'same-origin',
+				headers: {
+					'Content-Type': 'application/json',
+					accept: 'application/json'
+				},
+				body: JSON.stringify({
+					token: getCsrfToken()
+				})
+			});
+		})
 		.then(function handleResponse (res) {
 			if (!res.ok) {
 				throw new Error('could not subscribe to newsletter');
