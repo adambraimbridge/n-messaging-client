c6a5407ff0f3fca6740cc991bcdafb01cf8a0af1
Nick Ramsbottom <nick.ramsbottom@ft.com>
2020-06-10 10:19:37 +0100

    Only drop popup prompt cookie when dismissed 🐿 v2.13.0

--- client/components/bottom/marketing-popup-prompt/main.js
+++ client/components/bottom/marketing-popup-prompt/main.js
@@ -7,10 +7,14 @@ export default function (banner, done) {
 	const timeSet = cookies.get(POPUP_PROMPT_COOKIE_NAME);
 
 	if (timeSet === undefined) {
-		const expiryDate = new Date(new Date().getTime() + 10 * ONE_DAY_MS);
-		cookies.set(POPUP_PROMPT_COOKIE_NAME, 'OK', { domain: 'ft.com', expires: expiryDate });
 		banner.open();
 	}
+
+	const closeButton = banner.bannerElement.querySelector('.o-banner__close');
+	closeButton.addEventListener('click', function addCookie () {
+		const expiryDate = new Date(new Date().getTime() + 10 * ONE_DAY_MS);
+		cookies.set(POPUP_PROMPT_COOKIE_NAME, 'OK', { domain: 'ft.com', expires: expiryDate });
+	});
 
 	return done({ skip: true });
 }
