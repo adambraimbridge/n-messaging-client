d70bac008b547294616bad1f8d2fca66d05e0ba9
Nick Ramsbottom <nick.ramsbottom@ft.com>
2020-07-01 12:50:35 +0100

    Tidy up conditional statement 🐿 v2.13.0

--- client/components/bottom/remaining-articles/main.js
+++ client/components/bottom/remaining-articles/main.js
@@ -1,7 +1,5 @@
 export default function customSetup (banner, done, guruResult) {
-	const remainingArticles = guruResult.renderData.remainingArticles;
-
-	if(remainingArticles === 0) {
+	if (guruResult.renderData.remainingArticles === 0) {
 		return;
 	}
 
